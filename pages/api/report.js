7b991b344ecd5e0f267e26d9a7e3b56c37127b13
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-05-19 07:29:06 +0530

    fix(typo): change webhook title

--- pages/api/report.js
+++ pages/api/report.js
@@ -1,7 +1,7 @@
 export default async function reportAPI(req, res) {
   try {
     let params = {
-      username: "Contact Form Notif",
+      username: "Monotone Report",
       avatar_url: "",
       content: JSON.stringify(req.body)
     };
